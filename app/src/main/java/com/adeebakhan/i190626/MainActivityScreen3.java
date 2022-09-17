da3985dcb5a8023cc6b115299a96e6f2ac30bcb5
Adeeba Khan <i190626@nu.edu.pk>
2022-09-17 18:50:26 +0500

    Screen-3

--- app/src/main/java/com/adeebakhan/i190626/MainActivityScreen3.java
+++ app/src/main/java/com/adeebakhan/i190626/MainActivityScreen3.java
@@ -2,7 +2,11 @@ package com.adeebakhan.i190626;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivityScreen3 extends AppCompatActivity {
 
@@ -10,5 +14,31 @@ public class MainActivityScreen3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen3);
+
+        final Button button = findViewById(R.id.signup);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivityScreen3.this, MainActivityScreen2.class);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(intent);
+                    }
+                },1000);
+            }
+        });
+
+        final Button buttonSignin = findViewById(R.id.signin);
+        buttonSignin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivityScreen3.this, MainActivityScreen4.class);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(intent);
+                    }
+                },1000);
+            }
+        });
     }
 }
