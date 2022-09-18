c1317f7e12015f18dc9f133f9ba4d95f6b8fbaa2
Adeeba Khan <i190626@nu.edu.pk>
2022-09-18 16:23:51 +0500

    Screen-9

--- app/src/main/java/com/adeebakhan/i190626/MainActivityScreen9.java
+++ app/src/main/java/com/adeebakhan/i190626/MainActivityScreen9.java
@@ -2,7 +2,12 @@ package com.adeebakhan.i190626;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivityScreen9 extends AppCompatActivity {
 
@@ -10,5 +15,31 @@ public class MainActivityScreen9 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen9);
+
+        final ImageView back = findViewById(R.id.back_button);
+        back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivityScreen9.this, MainActivityScreen8.class);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(intent);
+                    }
+                },10);
+            }
+        });
+
+        final Button record = findViewById(R.id.screen9_record);
+        record.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivityScreen9.this, MainActivityScreen10.class);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(intent);
+                    }
+                },10);
+            }
+        });
     }
 }
