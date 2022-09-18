a86afae3d606f7f26b7e829b2ec727cc7e6a5a65
Adeeba Khan <i190626@nu.edu.pk>
2022-09-18 19:02:12 +0500

    Screen-16

--- app/src/main/java/com/adeebakhan/i190626/MainActivityScreen16.java
+++ app/src/main/java/com/adeebakhan/i190626/MainActivityScreen16.java
@@ -2,7 +2,11 @@ package com.adeebakhan.i190626;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivityScreen16 extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivityScreen16 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen16);
+
+        final ImageView search = findViewById(R.id.screen16_search);
+        search.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivityScreen16.this, MainActivityScreen17.class);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(intent);
+                    }
+                },10);
+            }
+        });
     }
 }
