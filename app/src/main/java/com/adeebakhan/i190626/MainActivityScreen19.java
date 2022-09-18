ea8bd97032c2c5f47a7ebbefe5658f510029cc9b
Adeeba Khan <i190626@nu.edu.pk>
2022-09-18 21:10:48 +0500

    Screen-19

--- app/src/main/java/com/adeebakhan/i190626/MainActivityScreen19.java
+++ app/src/main/java/com/adeebakhan/i190626/MainActivityScreen19.java
@@ -2,7 +2,12 @@ package com.adeebakhan.i190626;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivityScreen19 extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainActivityScreen19 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen19);
+
+        final ImageView liked = findViewById(R.id.screen19_liked);
+        liked.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivityScreen19.this, MainActivityScreen20.class);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(intent);
+                    }
+                },10);
+            }
+        });
     }
 }
