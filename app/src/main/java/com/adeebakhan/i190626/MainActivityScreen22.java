e104fdf0f4fe0e261df1ee81ee135f8b3bd23847
Adeeba Khan <i190626@nu.edu.pk>
2022-09-18 22:02:47 +0500

    Screen-22

--- app/src/main/java/com/adeebakhan/i190626/MainActivityScreen22.java
+++ app/src/main/java/com/adeebakhan/i190626/MainActivityScreen22.java
@@ -2,7 +2,11 @@ package com.adeebakhan.i190626;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivityScreen22 extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivityScreen22 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen22);
+
+        final ImageView back = findViewById(R.id.screen22_back);
+        back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivityScreen22.this, MainActivityScreen21.class);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(intent);
+                    }
+                },10);
+            }
+        });
     }
 }
