8b3d715e4b706ec89d0ded9157924a9a3ecaf794
Adeeba Khan <i190626@nu.edu.pk>
2022-09-18 11:38:23 +0500

    Screen-5

--- app/src/main/java/com/adeebakhan/i190626/MainActivityScreen5.java
+++ app/src/main/java/com/adeebakhan/i190626/MainActivityScreen5.java
@@ -2,7 +2,12 @@ package com.adeebakhan.i190626;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivityScreen5 extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainActivityScreen5 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen5);
+
+        final TextView songName = findViewById(R.id.song_name);
+        songName.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivityScreen5.this, MainActivityScreen6.class);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(intent);
+                    }
+                },10);
+            }
+        });
     }
 }
